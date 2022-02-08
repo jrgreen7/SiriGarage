2d262499d2f5a778a58d0ac628b419b770fd126e
James Green <jrgreen@sce.carleton.ca>
2022-02-07 19:25:13 -0500

    Update siriweb.py
    Updated and cleaned code. Also added ability to toggle the light on Door1.

--- siriweb.py
+++ siriweb.py
@@ -7,24 +7,37 @@ import socket
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(socket.gethostname() + ".local")
 
+# Define the pin numbers (C = closed sensor; O = open sensor; R = relay output)
+D1_C_PIN = 16
+D1_O_PIN = 18
+D2_C_PIN = 29
+D2_O_PIN = 31
+D3_C_PIN = 33
+D3_O_PIN = 37
+D1_R_PIN = 7
+D2_R_PIN = 11
+D3_R_PIN = 13  # Used to toggle light on Door 1 (connected via resistor)
+D4_R_PIN = 15
+
+
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)  # the pin numbers refer to the board connector not the chip
 GPIO.setwarnings(False)
-GPIO.setup(16, GPIO.IN, GPIO.PUD_UP) # Door 1 is Closed sensor
-GPIO.setup(18, GPIO.IN, GPIO.PUD_UP) # Door 1 is Open sensor
-GPIO.setup(29, GPIO.IN, GPIO.PUD_UP) # Door 2 is Closed sensor
-GPIO.setup(31, GPIO.IN, GPIO.PUD_UP) # Door 2 is Open sensor
-GPIO.setup(33, GPIO.IN, GPIO.PUD_UP) # Door 3 is Closed sensor
-GPIO.setup(37, GPIO.IN, GPIO.PUD_UP) # Door 3 is Open sensor
+GPIO.setup(D1_C_PIN, GPIO.IN, GPIO.PUD_UP) # Door 1 is Closed sensor
+GPIO.setup(D1_O_PIN, GPIO.IN, GPIO.PUD_UP) # Door 1 is Open sensor
+GPIO.setup(D2_C_PIN, GPIO.IN, GPIO.PUD_UP) # Door 2 is Closed sensor
+GPIO.setup(D2_O_PIN, GPIO.IN, GPIO.PUD_UP) # Door 2 is Open sensor
+GPIO.setup(D3_C_PIN, GPIO.IN, GPIO.PUD_UP) # Door 3 is Closed sensor
+GPIO.setup(D3_O_PIN, GPIO.IN, GPIO.PUD_UP) # Door 3 is Open sensor
 
-GPIO.setup(7, GPIO.OUT)			#Door 1 Relay to Open Door
-GPIO.output(7, GPIO.HIGH)
-GPIO.setup(11, GPIO.OUT)		#Door 2 Relay to Open Door
-GPIO.output(11, GPIO.HIGH)
-GPIO.setup(13, GPIO.OUT)		#Door 3 Relay to Open Door
-GPIO.output(13, GPIO.HIGH)
-GPIO.setup(15, GPIO.OUT)		#Not Used for the project
-GPIO.output(15, GPIO.HIGH)
+GPIO.setup(D1_R_PIN, GPIO.OUT)			#Door 1 Relay to Open Door
+GPIO.output(D1_R_PIN, GPIO.HIGH)
+GPIO.setup(D2_R_PIN, GPIO.OUT)		#Door 2 Relay to Open Door
+GPIO.output(D2_R_PIN, GPIO.HIGH)
+GPIO.setup(D3_R_PIN, GPIO.OUT)		#Door 3 Relay to Open Door (highjacked to toggle light)
+GPIO.output(D3_R_PIN, GPIO.HIGH)
+GPIO.setup(D4_R_PIN, GPIO.OUT)		#Not Used for the project
+GPIO.output(D4_R_PIN, GPIO.HIGH)
 
 from config import (
 	PORT,
@@ -88,7 +101,33 @@ print('')
 print('-------------------------------------------')
 
 
+# Helper function to activate one of the doors. 
+# door = door number (1, 2 or 3)
+def activateDoor(door):
+	door2Pin = {
+		1: D1_R_PIN,
+		2: D2_R_PIN,
+		3: D3_R_PIN
+	}
+	pin = door2Pin.get(door, 0)
 
+	if pin == 0:
+		print("Bad door number requested in activateDoor()")
+	else:
+		print("Activating Door", door)
+		GPIO.output(pin, GPIO.LOW)
+		time.sleep(1)
+		GPIO.output(pin, GPIO.HIGH)
+
+
+# Helper function to print to log file
+def printLOG(request, msg):
+	logfile = open("static/log.txt","a")
+	logfile.write(datetime.now().strftime("%Y/%m/%d -- %H:%M:%S  -- ") + request.environ['REMOTE_ADDR'] + " -- " + msg + "\n")
+	logfile.close()
+
+
+# Code to implement web HTML interface  (Flask address is /)
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
@@ -99,19 +138,13 @@ def index():
 		if code == PASSWORD and ENABLE_PASSWORD == "YES" and BadPassword <= 5:  # 12345678 is the Default Password that Opens Garage Door (Code if Password is Correct)
 			print("Door requested to open: " + Door_To_Open)
 			if Door_To_Open == "door1":
-				GPIO.output(7, GPIO.LOW)
-				time.sleep(1)
-				GPIO.output(7, GPIO.HIGH)
+				activateDoor(1)
 				time.sleep(2)
 			if Door_To_Open == "door2":
-				GPIO.output(11, GPIO.LOW)
-				time.sleep(1)
-				GPIO.output(11, GPIO.HIGH)
+				activateDoor(2)
 				time.sleep(2)
 			if Door_To_Open == "door3":
-				GPIO.output(13, GPIO.LOW)
-				time.sleep(1)
-				GPIO.output(13, GPIO.HIGH)
+				activateDoor(3)
 				time.sleep(2)
 
 		else:  		# 12345678 is the Password that Opens Garage Door (Code if Password is Incorrect)
@@ -119,23 +152,20 @@ def index():
 				code = "NULL"
 			else:
 				BadPassword += 1
-				logfile = open("static/log.txt","a")
-				logfile.write("     " + datetime.now().strftime(request.environ['REMOTE_ADDR'] + "     Password Entered: " + code + " -- %Y/%m/%d -- %H:%M  \n"))
-				logfile.close()
+				printLOG(request, "Bad Password Entered: " + code)
 				print(request.environ['REMOTE_ADDR'] + " -- " + str(BadPassword) + " wrong password(s) have been entered!")
 
 			if BadPassword > 5:
-				logfile = open("static/log.txt","a")
-				logfile.write("     " + datetime.now().strftime(request.environ['REMOTE_ADDR'] + "     Too Many Wrong Passwords, System Disabled.  -- %Y/%m/%d -- %H:%M  \n"))
-				logfile.close()
-				print("Garage Code Entered: " + code)
+				printLOG(request, "Too many wrong passwords, System disabled")
+				print("Too many wrong passwords, System disabled")
 
 
 	door1image = IMAGE_QUESTION		#Default Status, Door is questionable, so image is question mark
 	door2image = IMAGE_QUESTION		#Default Status, Door is questionable, so image is question mark
 	door3image = IMAGE_QUESTION		#Default Status, Door is questionable, so image is question mark
 
-	if GPIO.input(16) == GPIO.HIGH and GPIO.input(18) == GPIO.HIGH:
+	# Check status of Door1
+	if GPIO.input(D1_C_PIN) == GPIO.HIGH and GPIO.input(D1_O_PIN) == GPIO.HIGH:
 		if SENSORS_PER_DOOR == 1:
 			print("Door 1 is Open")
 			door1image = IMAGE_OPEN
@@ -144,17 +174,18 @@ def index():
 			door1image = IMAGE_QUESTION
 		Any_Door_Open = 1
 	else:
-		if GPIO.input(16) == GPIO.LOW:
+		if GPIO.input(D1_C_PIN) == GPIO.LOW:
 			print("Door 1 is Closed")
 			door1image = IMAGE_CLOSED
 			Any_Door_Open = 0
-		if GPIO.input(18) == GPIO.LOW:
+		if GPIO.input(D1_O_PIN) == GPIO.LOW:
 			print("Door 1 is Open")
 			door1image = IMAGE_OPEN
 			Any_Door_Open = 2
 
+	# Check status of Door2
 	if NUMBER_OF_DOORS > 1:
-		if GPIO.input(29) == GPIO.HIGH and GPIO.input(31) == GPIO.HIGH:
+		if GPIO.input(D2_C_PIN) == GPIO.HIGH and GPIO.input(D2_O_PIN) == GPIO.HIGH:
 			if SENSORS_PER_DOOR == 1:
 				print("Door 2 is Open")
 				door2image = IMAGE_OPEN
@@ -163,16 +194,17 @@ def index():
 				door2image = IMAGE_QUESTION
 			Any_Door_Open = Any_Door_Open + 1
 		else:
-			if GPIO.input(29) == GPIO.LOW:
+			if GPIO.input(D2_C_PIN) == GPIO.LOW:
 				print("Door 2 is Closed")
 				door2image = IMAGE_CLOSED
-			if GPIO.input(31) == GPIO.LOW:
+			if GPIO.input(D2_O_PIN) == GPIO.LOW:
 				print("Door 2 is Open")
 				door2image = IMAGE_OPEN
 				Any_Door_Open = Any_Door_Open + 2
 
+	# Check status of Door3
 	if NUMBER_OF_DOORS == 3:
-		if GPIO.input(33) == GPIO.HIGH and GPIO.input(37) == GPIO.HIGH:
+		if GPIO.input(D3_C_PIN) == GPIO.HIGH and GPIO.input(D3_O_PIN) == GPIO.HIGH:
 			if SENSORS_PER_DOOR == 1:
 				print("Door 3 is Open")
 				door3image = IMAGE_OPEN
@@ -181,10 +213,10 @@ def index():
 				door3image = IMAGE_QUESTION
 			Any_Door_Open = Any_Door_Open + 1
 		else:
-			if GPIO.input(33) == GPIO.LOW:
+			if GPIO.input(D3_C_PIN) == GPIO.LOW:
 				print("Door 3 is Closed")
 				door3image = IMAGE_CLOSED
-			if GPIO.input(37) == GPIO.LOW:
+			if GPIO.input(D3_O_PIN) == GPIO.LOW:
 				print("Door 3 is Open")
 				door3image = IMAGE_OPEN
 				Any_Door_Open = Any_Door_Open + 2
@@ -210,6 +242,7 @@ def index():
 		D3Name = DOOR_3_NAME)
 
 
+# Code associated with displaying current settings  (Flask address is /Settings)
 @app.route('/Settings', methods=['GET', 'POST'])
 def settings():
 	if request.method == 'POST':
@@ -266,6 +299,7 @@ def settings():
 	else:
 		return app.send_static_file('admin_login.html')
 
+# Code to capture new settings from the form and save them to config.py (Flask address is /ChangeSettings)
 @app.route('/ChangeSettings', methods=['POST'])
 def ChangeSettings():
 
@@ -310,6 +344,7 @@ def ChangeSettings():
 
 	return app.send_static_file('Settings_Saved.html')
 
+# Code to delete the log file (Flask address is /Delete_Log_File)
 @app.route('/Delete_Log_File', methods=['POST'])
 def Delete_Log_File():
 
@@ -324,6 +359,7 @@ def Delete_Log_File():
 	return app.send_static_file('Settings_Saved.html')
 
 
+#Code to get the status of all garage doors as a status string (Flask address is /GarageDoorStatus)
 @app.route('/Siri/GarageDoorStatus', methods=['GET'])
 def GarageDoorStatus():
 	siri_door1_message = ""
@@ -331,51 +367,52 @@ def GarageDoorStatus():
 	siri_door3_message = ""
 	Any_Door_Open = 0
 
-	if GPIO.input(16) == GPIO.HIGH and GPIO.input(18) == GPIO.HIGH: #Door 1 Unknown
+	if GPIO.input(D1_C_PIN) == GPIO.HIGH and GPIO.input(D1_O_PIN) == GPIO.HIGH: #Door 1 Unknown
 		if SENSORS_PER_DOOR == 1:
 			siri_door1_message = DOOR_1_NAME + " is open"
 		else:
 			siri_door1_message = DOOR_1_NAME + " is questionable"
 		Any_Door_Open = 1
 	else:
-		if GPIO.input(16) == GPIO.LOW: # Door 1 Closed
+		if GPIO.input(D1_C_PIN) == GPIO.LOW: # Door 1 Closed
 			siri_door1_message = ""
-		if GPIO.input(18) == GPIO.LOW: # Door 1 Open
+		if GPIO.input(D1_O_PIN) == GPIO.LOW: # Door 1 Open
 			siri_door1_message = DOOR_1_NAME + " is open"
 			Any_Door_Open = 1
 
 	if NUMBER_OF_DOORS > 1:
-		if GPIO.input(29) == GPIO.HIGH and GPIO.input(31) == GPIO.HIGH:
+		if GPIO.input(D2_C_PIN) == GPIO.HIGH and GPIO.input(D2_O_PIN) == GPIO.HIGH:
 			if SENSORS_PER_DOOR == 1:
 				siri_door2_message = DOOR_2_NAME + " is open"
 			else:
 				siri_door2_message = DOOR_2_NAME + " is questionable"
 			Any_Door_Open = Any_Door_Open + 1
 		else:
-			if GPIO.input(29) == GPIO.LOW:
-				siri_door1_message = ""
-			if GPIO.input(31) == GPIO.LOW:
+			if GPIO.input(D2_C_PIN) == GPIO.LOW:
+				siri_door2_message = ""
+			if GPIO.input(D2_O_PIN) == GPIO.LOW:
 				siri_door2_message = DOOR_2_NAME + " is open"
 				Any_Door_Open = Any_Door_Open + 1
 
 	if NUMBER_OF_DOORS == 3:
-		if GPIO.input(33) == GPIO.HIGH and GPIO.input(37) == GPIO.HIGH:
+		if GPIO.input(D3_C_PIN) == GPIO.HIGH and GPIO.input(D3_O_PIN) == GPIO.HIGH:
 			if SENSORS_PER_DOOR == 1:
 				siri_door3_message = DOOR_3_NAME + " is open"
 			else:
 				siri_door3_message = DOOR_3_NAME + " is questionable"
 			Any_Door_Open = Any_Door_Open + 1
 		else:
-			if GPIO.input(33) == GPIO.LOW:
+			if GPIO.input(D3_C_PIN) == GPIO.LOW:
 				siri_door3_message = ""
-			if GPIO.input(37) == GPIO.LOW:
+			if GPIO.input(D3_O_PIN) == GPIO.LOW:
 				siri_door3_message = DOOR_1_NAME + " is open"
 				Any_Door_Open = Any_Door_Open + 1
 
-	siri_message = ""
 	if Any_Door_Open == 0:
 		return 'All Doors are Closed'
-	if Any_Door_Open != 0:
+	return ", ".join([siri_door1_message, siri_door2_message, siri_door3_message])
+
+"""	if Any_Door_Open != 0:
 
 		if siri_door1_message != "":
 			siri_message = siri_door1_message
@@ -391,86 +428,86 @@ def GarageDoorStatus():
 				siri_message = siri_message + ', ' + siri_door3_message
 
 		return siri_message
+"""
 
 
+# Code to respond to Siri requests to open/close garage doors  (Flask address is /Siri/Garage)
 @app.route('/Siri/Garage', methods=['POST'])
 def GarageSiri():
-	ps = request.form['ps']
-	what_door = request.form['door']
-	dowhat = request.form['dowhat']
+	# Get the 3 field values from the form or POST request
+	ps = request.form.get('ps',"NA")
+	what_door = request.form.get('door',"NA")
+	dowhat = request.form.get('dowhat',"NA")
 
+	if ps == "NA":
+		printLOG(request, "GarageSiri():Password not retrieved. Using NA")
+	if what_door == "NA":
+		printLOG(request, "GarageSiri():door not retrieved. Using NA")
+	if dowhat == "NA":
+		printLOG(request, "GarageSiri():dowhat not retrieved. Using NA")
 
 	if ps == SIRI_PASSWORD:
-		logfile = open("static/log.txt","a")
-		logfile.write(datetime.now().strftime("%Y/%m/%d -- %H:%M:%S  -- " + request.environ['REMOTE_ADDR'] + " -- Garage Door Operated via Siri  \n"))
-		logfile.close()
+		printLOG(request, "GarageSiri():Garage Door Operated via Siri") 
 
+		if dowhat == "Light":
+			printLOG(request, "Toggling light.")
+			activateDoor(3)
+			return 'Garage Light Toggled'
 		if what_door == "Door1" and dowhat == "Open":
-			if GPIO.input(16) == GPIO.LOW:
-				print("Door 1 is currently Closed, let's open it.")
-				GPIO.output(7, GPIO.LOW)
-				time.sleep(1)
-				GPIO.output(7, GPIO.HIGH)
+			if GPIO.input(D1_C_PIN) == GPIO.LOW:
+				printLOG(request, "Door 1 is currently Closed, let's open it.")
+				activateDoor(1)
 				return 'Garage Door Opening'
-			if GPIO.input(16) == GPIO.HIGH:
-				print("Garage is already open, do nothing.")
+			if GPIO.input(D1_C_PIN) == GPIO.HIGH:
+				printLOG(request, "Garage is already open, do nothing.")
 				return 'Door 1 is already open'
 		if what_door == "Door1" and dowhat == "Close":
-			if GPIO.input(18) == GPIO.LOW:
-				print("Garage is currently Open, let's close it.")
-				GPIO.output(7, GPIO.LOW)
-				time.sleep(1)
-				GPIO.output(7, GPIO.HIGH)
+			if GPIO.input(D1_O_PIN) == GPIO.LOW:
+				printLOG(request, "Garage is currently Open, let's close it.")
+				activateDoor(1)
 				return 'Garage Door Closing'
-			if GPIO.input(18) == GPIO.HIGH:
-				print("Garage is already closed, do nothing.")
+			if GPIO.input(D1_O_PIN) == GPIO.HIGH:
+				printLOG(request, "Garage is already closed, do nothing.")
 				return 'Door 1 is already closed'
 
 		if what_door == "Door2" and dowhat == "Open":
-			if GPIO.input(29) == GPIO.LOW:
-				print("Door 2 is currently Closed, let's open it.")
-				GPIO.output(11, GPIO.LOW)
-				time.sleep(1)
-				GPIO.output(11, GPIO.HIGH)
+			if GPIO.input(D2_C_PIN) == GPIO.LOW:
+				printLOG(request, "Door 2 is currently Closed, let's open it.")
+				activateDoor(2)
 				return 'Garage Door Opening'
-			if GPIO.input(29) == GPIO.HIGH:
-				print("Garage is already open, do nothing.")
+			if GPIO.input(D2_C_PIN) == GPIO.HIGH:
+				printLOG(request, "Garage is already open, do nothing.")
 				return 'Door 2 is already open'
 		if what_door == "Door2" and dowhat == "Close":
-			if GPIO.input(31) == GPIO.LOW:
-				print("Garage is currently Open, let's close it.")
-				GPIO.output(11, GPIO.LOW)
-				time.sleep(1)
-				GPIO.output(11, GPIO.HIGH)
+			if GPIO.input(D2_O_PIN) == GPIO.LOW:
+				printLOG(request, "Garage is currently Open, let's close it.")
+				activateDoor(2)
 				return 'Garage Door Closing'
-			if GPIO.input(31) == GPIO.HIGH:
-				print("Garage is already closed, do nothing.")
+			if GPIO.input(D2_O_PIN) == GPIO.HIGH:
+				printLOG(request, "Garage is already closed, do nothing.")
 				return 'Door 2 is already closed'
 
 		if what_door == "Door3" and dowhat == "Open":
-			if GPIO.input(33) == GPIO.LOW:
-				print("Door 2 is currently Closed, let's open it.")
-				GPIO.output(13, GPIO.LOW)
-				time.sleep(1)
-				GPIO.output(13, GPIO.HIGH)
+			if GPIO.input(D3_C_PIN) == GPIO.LOW:
+				printLOG(request, "Door 2 is currently Closed, let's open it.")
+				activateDoor(3)
 				return 'Garage Door Opening'
-			if GPIO.input(33) == GPIO.HIGH:
-				print("Garage is already open, do nothing.")
+			if GPIO.input(D3_C_PIN) == GPIO.HIGH:
+				printLOG(request, "Garage is already open, do nothing.")
 				return 'Door 2 is already open'
 		if what_door == "Door3" and dowhat == "Close":
-			if GPIO.input(37) == GPIO.LOW:
-				print("Garage is currently Open, let's close it.")
-				GPIO.output(13, GPIO.LOW)
-				time.sleep(1)
-				GPIO.output(13, GPIO.HIGH)
+			if GPIO.input(D3_O_PIN) == GPIO.LOW:
+				printLOG(request, "Garage is currently Open, let's close it.")
+				activateDoor(3)
 				return 'Garage Door Closing'
-			if GPIO.input(37) == GPIO.HIGH:
-				print("Garage is already closed, do nothing.")
+			if GPIO.input(D3_O_PIN) == GPIO.HIGH:
+				printLOG(request, "Garage is already closed, do nothing.")
 				return 'Door 2 is already closed'
 
-		return 'We have a problem with your Siri shortcut entries' 
+		return 'GarageSiri() DEBUG: We have a problem with your Siri shortcut entries' 
 	else:
-		return 'We have a problem'
+		printLOG(request, "GarageSiri(): SIRI Password rejected\nSIRI Password entered: " + ps)
+		return "GarageSiri(): We have a problem with the password."
 
 @app.route('/stylesheet.css')
 def stylesheet():
